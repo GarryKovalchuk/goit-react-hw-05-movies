0ce82ab59403f5fae58af86e570d28c5ca9ff273
Garry <vorzel47051@gmail.com>
2024-02-02 21:50:37 +0200

    react05movies

---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,32 @@
+import { fetchMovieReviews } from '../ServiceAPI/api';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import css from './Review.module.css';
+
+const Reviews = () => {
+  const { movieId } = useParams('movieId');
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetchMovieReviews(movieId).then(setReviews);
+  }, [movieId]);
+
+  return (
+    <div className={css.ReviewsContainer}>
+      {reviews.length > 0 ? (
+        <ul>
+          {reviews.map(({ id, author, content }) => (
+            <li key={id}>
+              <h3>{author}</h3>
+              <p>{content}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>We don`t have any reviews for this movie</p>
+      )}
+    </div>
+  );
+};
+
+export default Reviews;
